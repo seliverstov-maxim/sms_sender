7840524f8be39b35c3271f0b831c4fd16098d476
Seliverstov Maxim <maxim.web.developer@gmail.com>
2013-07-14 22:52:04 +0400

    Modify test.

--- spec/sms_gear_api_spec.rb
+++ spec/sms_gear_api_spec.rb
@@ -2,23 +2,23 @@ require 'spec_helper'
 
 describe SmsGearApi do
   before :all do
-    @sms_gear_api = SmsGearApi::Base.new login: '79250_test', passw: '123456'
+    @sms_gear_api = SmsGearApi::Base.new login: '79250_test', passw: '123456', url: 'http://gates.smschimp.ru/http/gate.cgi'
   end
 
   it 'should send sms' do
     params = {
-      target: '79084774210',
+      target: '79041855645',
       sender: 'maxim',
-      body: 'hello world!'
+      message: 'hello world!'
     }
     @sms_gear_api.send_sms(params).should be_true
   end
 
   it 'should send bulk sms' do
     params = {
-      target: ['79084774210', '79084774210'],
+      target: ['79041855645', '79084774210'],
       sender: 'maxim',
-      body: 'hello world!'
+      message: 'hello world!'
     }
     @sms_gear_api.send_sms(params).should be_true
   end
